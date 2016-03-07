75e462753c5a1d2b2f906a034d7bb7543ed401f3
wuyuanyi <ywu647@uwo.ca>
2016-03-07 09:46:30 +0000

    change port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use("/file", feathers.static("static"));
 app.use("/version", function(req, res) {
     res.sendFile(__dirname + "/static/version.txt");
 });
-app.listen(8080);
-//https://static-updater-wuyuanyi.c9users.io/file/version.txt
+app.listen(8000);
+//https://static-updater-wuyuanyi.c9users.io/version
 //https://static-updater-wuyuanyi.c9users.io/file/update.zip
