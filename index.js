5b952a5ccfc31e1c22e0c0e327afd6051f8f3916
wuyuanyi <ywu647@uwo.ca>
2016-03-08 03:21:37 +0000

    add md5

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var feathers = require("feathers");
-
+var md5File = require('md5-file');
 var app = feathers();
 
 app.use("/file", feathers.static("static"));
-app.use("/version", function(req, res) {
+app.get("/version", function(req, res) {
     res.sendFile(__dirname + "/static/version.txt");
 });
-app.listen(8000);
+app.get("/md5/:filename", function(req, res) {
+    md5File('./static/' + req.params.filename, function(error, sum) {
+        if (error) {
+            res.status(500).json(error);
+        } else {
+            res.send(sum);
+        }
+    });
+});
+app.listen(8000, function(){
+    console.log("Listening");
+});
 //https://static-updater-wuyuanyi.c9users.io/version
 //https://static-updater-wuyuanyi.c9users.io/file/update.zip
